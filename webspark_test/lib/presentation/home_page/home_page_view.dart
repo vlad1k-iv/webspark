0817069e6167ddbddd0afc3834b274618acc0b60
Vlad <vlad.flutter.bsx@gmail.com>
2024-08-20 11:56:12 +0300

    main page layout

--- webspark_test/lib/presentation/home_page/home_page_view.dart
+++ webspark_test/lib/presentation/home_page/home_page_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:webspark_test/core/extensions.dart';
+import 'package:webspark_test/core/functions.dart';
+import 'package:webspark_test/presentation/resources/colors_manager.dart';
+import 'package:webspark_test/presentation/resources/text_styles.dart';
+import 'package:webspark_test/presentation/widgets/app_elevated_button.dart';
 
 class HomePageView extends StatefulWidget {
   const HomePageView({super.key});
@@ -8,8 +13,70 @@ class HomePageView extends StatefulWidget {
 }
 
 class _HomePageViewState extends State<HomePageView> {
+  final _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: () {
+        unFocus(context);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: false,
+          title: const Text(
+            'Home screen',
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const Text(
+                  'Set valid API base URL in order to continue',
+                  style: TextStyles.bodyMedium,
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Icon(
+                      Icons.compare_arrows_rounded,
+                      size: 30,
+                      color: ColorsManager.gray,
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        decoration: const InputDecoration(hintText: 'Link'),
+                        controller: _controller,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Spacer(),
+                SizedBox(
+                  width: context.screenWidth - 32,
+                  height: 55,
+                  child: AppElevatedButton(
+                    onTap: () {},
+                    text: 'Start counting proccess',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
